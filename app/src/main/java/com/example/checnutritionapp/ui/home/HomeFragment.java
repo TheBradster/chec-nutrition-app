b10e1dc27a1077b8f4a3e3a19fcd87031528ef48
TheBradster <bjmrtin2@memphis.edu>
2020-03-08 18:23:39 -0500

    Added MealActivity + ImageButton refs

--- app/src/main/java/com/example/checnutritionapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/checnutritionapp/ui/home/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -16,6 +17,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.checnutritionapp.MainActivity;
+import com.example.checnutritionapp.MealActivity;
 import com.example.checnutritionapp.PlaceOrderActivity;
 import com.example.checnutritionapp.R;
 import com.example.checnutritionapp.model.Meal;
@@ -38,6 +40,79 @@ public class HomeFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        // Meal buttons
+        final ImageButton m1 = (ImageButton) root.findViewById(R.id.imageButton2);
+        m1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m2 = (ImageButton) root.findViewById(R.id.imageButton3);
+        m2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m3 = (ImageButton) root.findViewById(R.id.imageButton4);
+        m3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m4 = (ImageButton) root.findViewById(R.id.imageButton5);
+        m4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m5 = (ImageButton) root.findViewById(R.id.imageButton6);
+        m5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m6 = (ImageButton) root.findViewById(R.id.imageButton7);
+        m6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m7 = (ImageButton) root.findViewById(R.id.imageButton8);
+        m7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton m8 = (ImageButton) root.findViewById(R.id.imageButton9);
+        m8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MealActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/example/checnutritionapp/ui/meal/MealViewModel.java
@@ -0,0 +1,7 @@
+package com.example.checnutritionapp.ui.meal;
+
+import androidx.lifecycle.ViewModel;
+
+public class MealViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
