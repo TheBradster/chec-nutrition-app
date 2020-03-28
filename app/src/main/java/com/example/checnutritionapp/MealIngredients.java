d79acfc5c0710d1658a0fc33e575f167dbf47593
TheBradster <bjmrtin2@memphis.edu>
2020-03-28 14:20:35 -0500

    Added MealIngredients fragment

---
+++ app/src/main/java/com/example/checnutritionapp/MealIngredients.java
@@ -0,0 +1,36 @@
+package com.example.checnutritionapp;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MealIngredients extends Fragment {
+
+    private MealIngredientsViewModel mViewModel;
+
+    public static MealIngredients newInstance() {
+        return new MealIngredients();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.meal_ingredients_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MealIngredientsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
