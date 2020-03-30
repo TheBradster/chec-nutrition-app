e82b9053d777e977d4691103daffe34719a05dd5
TheBradster <bjmrtin2@memphis.edu>
2020-03-30 15:48:20 -0500

    Add MealProcedureFragment to code

---
+++ app/src/main/java/com/example/checnutritionapp/MealProcedureFragment.java
@@ -0,0 +1,36 @@
+package com.example.checnutritionapp;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MealProcedureFragment extends Fragment {
+
+    private MealProcedureViewModel mViewModel;
+
+    public static MealProcedureFragment newInstance() {
+        return new MealProcedureFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.meal_procedure_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MealProcedureViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
