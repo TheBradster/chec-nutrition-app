9449fd48d625c951f591d3da1eec7fef14b5d6a4
James Willson <james.willson@att.net>
2020-04-19 16:58:42 -0500

    Started view, monday completed

--- app/src/main/java/com/example/checnutritionapp/ui/queue/QueueFragment.java
+++ app/src/main/java/com/example/checnutritionapp/ui/queue/QueueFragment.java
@@ -23,70 +23,6 @@ public class QueueFragment extends Fragment {
 
     private QueueViewModel queueViewModel;
 
-    private Date Mon= new SimpleDateFormat("MM/dd/yyyy").parse("02/03/2020");
-    private Date Tues= new SimpleDateFormat("MM/dd/yyyy").parse("02/04/2020");
-    private Date Wed= new SimpleDateFormat("MM/dd/yyyy").parse("02/05/2020");
-    private Date Thurs= new SimpleDateFormat("MM/dd/yyyy").parse("02/06/2020");
-
-
-    private Meal[] Meals={new Meal (1,"[Mon1]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(2, "[Mon2]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(3, "[Tues1]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(4,"[Tues2]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(5,"[Wed1]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(6,"[Wed2]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(7,"[Thu1]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1]),
-                          new Meal(8,"[Thu2]",5, "Sample description", "Sample Procedure", "0hr, 0m", new String[1])};
-
-
-    private Location [] Locations={new Location("Claremont High","1601 N Indian Hill Blvd, Claremont, CA 91711"),
-            new Location("Ohio State University","Columbus, OH 43210"),
-            new Location("University of Memphis","3720 Alumni Ave, Memphis, TN 38152"),
-            new Location("University of Cincinnati","2600 Clifton Ave, Cincinnati, OH 45221")};
-
-
-    private Order [] Orders={new Order (Mon,Meals),new Order (Tues,Meals),new Order (Wed,Meals),new Order (Thurs,Meals),};
-
-    private Ticket [] Tickets  =Orders[0].getTickets();
-
-
-
-    private String mon;
-    private Button Monday;
-    private int M1=2;
-    private int M2=0;
-    private double TotalM;
-
-
-    private String tues;
-    private Button Tuesday;
-    private int T1=0;
-    private int T2=0;
-    private double TotalT;
-
-
-
-    private String wednes;
-    private Button Wednesday;
-    private int W1=0;
-    private int W2=0;
-    private double TotalW;
-
-
-
-    private String thurs;
-    private Button Thursday;
-    private int R1=0;
-    private int R2=0;
-    private double TotalR;
-
-
-
-
-    private double TotalMoney;
-
-
-
     public QueueFragment() throws ParseException {
     }
 
@@ -96,56 +32,6 @@ public class QueueFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Tickets[0].changeServing(M1);
-        Tickets[1].changeServing(M2);
-
-        Tickets[2].changeServing(T1);
-        Tickets[3].changeServing(T2);
-
-        Tickets[4].changeServing(W1);
-        Tickets[5].changeServing(W2);
-
-        Tickets[6].changeServing(R1);
-        Tickets[7].changeServing(R2);
-
-        TotalM=Tickets[0].total()+Tickets[1].total();
-        TotalT=Tickets[2].total()+Tickets[3].total();
-        TotalW=Tickets[4].total()+Tickets[5].total();
-        TotalR=Tickets[6].total()+Tickets[7].total();
-
-        TotalMoney=TotalM+TotalT+TotalW+TotalR;
-
-        String TM=String.format("%.2f",TotalMoney);
-        String TMon=String.format("%.2f",TotalM);
-        String TTues=String.format("%.2f",TotalT);
-        String TWed=String.format("%.2f",TotalW);
-        String TThurs=String.format("%.2f",TotalR);
-
-          mon= "\n "+Orders[0].getReadyDate()+"                             Total: $"+TMon+"\n\n "+Meals[0].getName()+": "+Tickets[0].numberOfServings()+"\n                          Pickup: "+Locations[0].getName()+" \n "+Meals[1].getName()+": "+Tickets[1].numberOfServings();
-
-          tues= "\n "+Orders[1].getReadyDate()+"                             Total: $"+TTues+"\n\n "+Meals[2].getName()+": "+Tickets[2].numberOfServings()+"\n                          Pickup: "+Locations[1].getName()+" \n "+Meals[3].getName()+": "+Tickets[3].numberOfServings();
-
-          wednes= "\n "+Orders[2].getReadyDate()+"                             Total: $"+TWed+"\n\n "+Meals[4].getName()+": "+Tickets[4].numberOfServings()+"\n                          Pickup: "+Locations[2].getName()+" \n "+Meals[5].getName()+": "+Tickets[5].numberOfServings();
-
-          thurs= "\n "+Orders[3].getReadyDate()+"                             Total: $"+TThurs+"\n\n "+Meals[6].getName()+": "+Tickets[6].numberOfServings()+"\n                          Pickup: "+Locations[3].getName()+" \n "+Meals[7].getName()+": "+Tickets[7].numberOfServings();
-
-
-        Monday = getView().findViewById(R.id.button7);
-        Monday.setText(mon);
-
-
-        Tuesday = getView().findViewById(R.id.button6);
-        Tuesday.setText(tues);
-
-        Wednesday = getView().findViewById(R.id.button2);
-        Wednesday.setText(wednes);
-
-        Thursday = getView().findViewById(R.id.button8);
-        Thursday.setText(thurs);
-
-        TextView textView=getView().findViewById(R.id.textView2);
-
-        textView.setText("$ "+TM);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
