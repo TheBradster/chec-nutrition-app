ce5e8e9b33efd50231efe69bc4c5e1b059304e6f
Deorcan <tmlmqist@memphis.edu>
2020-04-21 11:54:06 -0500

    User Profile View Boss 2

--- app/src/main/java/com/example/checnutritionapp/ui/user_profile/UserProfileFragment.java
+++ app/src/main/java/com/example/checnutritionapp/ui/user_profile/UserProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.checnutritionapp.ui.user_profile;
 
 import android.os.Bundle;
+import android.util.StringBuilderPrinter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,7 +72,7 @@ public class UserProfileFragment extends Fragment {
 
         bmi=(lbs/((foot*12+inch)*(foot*12+inch)))*703;
 
-        BMI="BMI: "+bmi;
+        BMI="BMI: "+ String.format("%.1f",bmi);
 
         fbg=108;
 
@@ -79,7 +80,7 @@ public class UserProfileFragment extends Fragment {
 
         a1c=(46.7+fbg)/28.7;
 
-        A1C="A1C: "+a1c;
+        A1C="A1C: "+String.format("%.1f",a1c);
 
         cholest=150;
 
@@ -87,17 +88,45 @@ public class UserProfileFragment extends Fragment {
 
         TextView name=getView().findViewById(R.id.textView8);
 
+        TextView o=getView().findViewById(R.id.textView11);
+
+        TextView f=getView().findViewById(R.id.textView12);
+
         TextView a=getView().findViewById(R.id.textView5);
 
         TextView b=getView().findViewById(R.id.textView7);
+
+        TextView h=getView().findViewById(R.id.textView10);
+
+        TextView fb=getView().findViewById(R.id.textView13);
+
+        TextView a1=getView().findViewById(R.id.textView14);
+
+        TextView l=getView().findViewById(R.id.textView17);
+
+        TextView c=getView().findViewById(R.id.textView16);
 
         ImageView image =getView().findViewById(R.id.imageView3);
 
         name.setText(Name);
 
+        o.setText(occ);
+
+        f.setText(Fam);
+
         a.setText(Age);
 
         b.setText(BMI);
+
+        h.setText(height);
+
+        fb.setText(FBG);
+
+        a1.setText(A1C);
+
+        l.setText(weight);
+
+        c.setText(Cholest);
 
         image.setImageResource(R.drawable.percyjackson);
 
