caa440ebe95c27a2574b606d315c45d2490c279f
nrdmraju <46762136+nrdmraju@users.noreply.github.com>
2020-04-23 15:38:47 -0500

    added code for the button to link to your week

--- app/src/main/java/com/example/checnutritionapp/ui/Login.java
+++ app/src/main/java/com/example/checnutritionapp/ui/Login.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.checnutritionapp.R;
+import com.example.checnutritionapp.ui.your_week.YourWeekFragment;
 
 public class Login extends Fragment {
 
@@ -23,7 +27,22 @@ public class Login extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.login_fragment, container, false);
+
+        View v = inflater.inflate(R.layout.login_fragment, container, false);
+        Button openLogin = (Button) v.findViewById(R.id.sign_in);
+        openLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                YourWeekFragment yourWeekFragment = new YourWeekFragment();
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction trans = manager.beginTransaction();
+                trans.replace(R.id.frameLayout, yourWeekFragment);
+                trans.addToBackStack(null);
+                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
+            }
+        });
+        return v;
+
     }
 
     @Override
