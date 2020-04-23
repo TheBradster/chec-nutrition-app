0a5f22e2fa7ca4e62c4ff0497d9131ddc63609b8
nrdmraju <46762136+nrdmraju@users.noreply.github.com>
2020-04-23 15:47:33 -0500

    created the majority of the view for the edit info page

---
+++ app/src/main/java/com/example/checnutritionapp/ui/EditProfileInfoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.checnutritionapp.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditProfileInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
